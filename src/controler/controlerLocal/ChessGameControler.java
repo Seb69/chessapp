5305653484bea525b8e0b6e1eb583980c9f949cc
ANDRE <sebastien.andre.seb@gmail.com>
2015-11-13 16:03:56 +0100

    working project

---
+++ src/controler/controlerLocal/ChessGameControler.java
@@ -0,0 +1,23 @@
+package controler.controlerLocal;
+
+import model.Coord;
+import model.observable.ChessGame;
+
+public class ChessGameControler {
+
+	public ChessGameControler(ChessGame chessGame) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void move(Coord coord, Coord coord2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
